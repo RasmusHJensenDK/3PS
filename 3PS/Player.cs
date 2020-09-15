2551446d7361b764efb6048a436511a27feca266
RasmusJensen101 <rasmus.jensen@hessel.dk>
2020-09-15 10:49:44 +0200

    Small updates.

--- 3PS/Player.cs
+++ 3PS/Player.cs
@@ -8,15 +8,17 @@ namespace _3PS
     {
         private bool playerturn { get; set; }
         private string playerName;
-        private Token[] token = new Token[3];
+        private Token token;
+        List<object> playertokens = new List<object>();
         public Player(string playerName, bool playerturn, Token token)
         {
             this.playerName = playerName;
             this.playerturn = playerturn;
-
+            this.token = token;
             for(int i = 0; i < 3; i++)
             {
-                this.token[i] = token;
+                token = new Token();
+                playertokens.Add(token);
             }
         }
         public string GetPlayerName()
